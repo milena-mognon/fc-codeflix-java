480ed99f71f78dd30923ce837500b1dfcaede538
milena-mognon <milenamognon@gmail.com>
2023-08-06 16:56:25 -0300

    refactor: created no stack trace exception

---
+++ admin-catalog/domain/src/main/java/com/fullcyccle/admin/catalogo/domain/exceptions/NoStackTraceException.java
@@ -0,0 +1,11 @@
+package com.fullcyccle.admin.catalogo.domain.exceptions;
+
+public class NoStackTraceException extends RuntimeException {
+  public NoStackTraceException(final String message) {
+    this(message, null);
+  }
+  
+  public NoStackTraceException(final String message, final Throwable cause) {
+    super(message, cause, true, false);
+  }
+}
