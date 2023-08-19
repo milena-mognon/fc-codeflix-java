e317cc09d26460bc5018ec2948202642172717aa
milena-mognon <milenamognon@gmail.com>
2023-08-19 16:00:09 -0300

    test: TDD delete category tests

---
+++ admin-catalog/application/src/test/java/com/fullcyccle/admin/catalogo/application/category/delete/DeleteCategoryUseCaseTest.java
@@ -0,0 +1,68 @@
+package com.fullcyccle.admin.catalogo.application.category.delete;
+
+import com.fullcyccle.admin.catalogo.domain.category.Category;
+import com.fullcyccle.admin.catalogo.domain.category.CategoryGateway;
+import com.fullcyccle.admin.catalogo.domain.category.CategoryID;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class DeleteCategoryUseCaseTest {
+  
+  @InjectMocks
+  private DefaultDeleteCategoryUseCase useCase;
+  
+  @Mock
+  private CategoryGateway categoryGateway;
+  
+  @BeforeEach
+  void cleanUp() {
+    reset(categoryGateway);
+  }
+  
+  @Test
+  public void givenAValidId_whenCallsDeleteCategory_shouldBeOk() {
+    final var aCategory = Category.newCategory("Filme", "A categoria mais assistida", true);
+    final var expectedId = aCategory.getId();
+    
+    // delete é um método void, por isso não precisa fazer um código para mocar o comportamento
+    // ou é possível mocar da seguinte forma
+    doNothing().when(categoryGateway).deleteById(eq(expectedId));
+    
+    Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue());
+    
+    verify(categoryGateway, times(1)).deleteById(eq(expectedId));
+  }
+  
+  @Test
+  public void givenAInvalidId_whenCallsDeleteCategory_shouldBeOk() {
+    final var expectedId = CategoryID.from("123");
+    
+    doNothing().when(categoryGateway).deleteById(eq(expectedId));
+    
+    Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue());
+    
+    verify(categoryGateway, times(1)).deleteById(eq(expectedId));
+  }
+  
+  @Test
+  public void givenAValidId_whenGatewayThrowsError_shouldReturnException() {
+    final var aCategory = Category.newCategory("Filme", "A categoria mais assistida", true);
+    final var expectedId = aCategory.getId();
+    doThrow(new IllegalStateException("Gateway Error"))
+        .when(categoryGateway).deleteById(eq(expectedId));
+    
+    Assertions.assertThrows(IllegalStateException.class, () -> useCase.execute(expectedId.getValue());
+    
+    verify(categoryGateway, times(1)).deleteById(eq(expectedId));
+  }
+  
+}
