c62d0466e9a6714603ff8d60f535f2595fe2b281
milena-mognon <milenamognon@gmail.com>
2023-08-20 16:41:02 -0300

    chore: springboot config

--- admin-catalog/infrastructure/src/main/java/com/fullcyccle/admin/catalogo/infrastructure/Main.java
+++ admin-catalog/infrastructure/src/main/java/com/fullcyccle/admin/catalogo/infrastructure/Main.java
@@ -4,6 +4,7 @@ import com.fullcyccle.admin.catalogo.application.UseCase;
 import com.fullcyccle.admin.catalogo.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 // muito importante adicionar essa anotação, o spring não irá funcionar sem ela
 @SpringBootApplication
@@ -14,6 +15,9 @@ public class Main {
      * SpringApplication deve receber uma classe que será utilizada para executar reflections
      * e pegar as configurações que devem ser utilizadas na hora de subir a aplicação
      * */
+    // System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "development");
+    System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+    
     SpringApplication.run(WebServerConfig.class, args);
   }
 }
