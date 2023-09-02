5cbd3ff5c961c851cbf0e8683444988eaab27752
milena-mognon <milenamognon@gmail.com>
2023-09-02 16:41:36 -0300

    feat: test database h2 config

--- admin-catalog/infrastructure/src/test/java/com/fullcyccle/admin/catalogo/infrastructure/MainTest.java
+++ admin-catalog/infrastructure/src/test/java/com/fullcyccle/admin/catalogo/infrastructure/MainTest.java
@@ -3,10 +3,12 @@ package com.fullcyccle.admin.catalogo.infrastructure;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Driver;
+
 public class MainTest {
-    @Test
-    public void testMain() {
-        Assertions.assertNotNull(new Main());
-        Main.main(new String[]{});
-    }
+  @Test
+  public void testMain() {
+    Assertions.assertNotNull(new Main());
+    Main.main(new String[]{});
+  }
 }
